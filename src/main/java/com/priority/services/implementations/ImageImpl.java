49eecf80c36265b15dfc4482f3bf046c3f71cb4f
IulianBotnari <iulianbotnari93@gmail.com>
2026-04-28 11:51:47 +0200

    add ImageImpl / add UserImpl / add TaskImpl

---
+++ src/main/java/com/priority/services/implementations/ImageImpl.java
@@ -0,0 +1,41 @@
+package com.priority.services.implementations;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.priority.dto.input.ImageRequest;
+import com.priority.dto.output.ImageResponse;
+import com.priority.services.interfaces.InterfaceImageService;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class ImageImpl implements InterfaceImageService{
+
+	@Override
+	public ImageResponse getById(ImageRequest request) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<ImageResponse> listAll() throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void create(ImageRequest request) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void delete(ImageRequest request) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ src/main/java/com/priority/services/implementations/TaskImpl.java
@@ -0,0 +1,38 @@
+package com.priority.services.implementations;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.priority.dto.input.TaskRequest;
+import com.priority.dto.output.TaskResponse;
+import com.priority.services.interfaces.InterfaceTaskService;
+
+@Service
+public class TaskImpl implements InterfaceTaskService{
+
+	@Override
+	public TaskResponse getById(TaskRequest request) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<TaskResponse> listAll() throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void create(TaskRequest request) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void delete(TaskRequest request) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
