7e769d7209cdaf2cc82314c7f3982bb55a8f163e
IulianBotnari <iulianbotnari93@gmail.com>
2026-04-28 10:26:02 +0200

    add task entity / create relation between user and task / add starter bean validator

--- src/main/java/com/priority/PriorityApplication.java
+++ src/main/java/com/priority/PriorityApplication.java
@@ -2,8 +2,10 @@ package com.priority;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class PriorityApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/priority/models/Task.java
+++ src/main/java/com/priority/models/Task.java
@@ -1,22 +1,22 @@
 package com.priority.models;
-
-import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.util.List;
 
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EntityListeners;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -29,45 +29,33 @@ import lombok.Setter;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-@Table(name = "users")
+@Table(name = "tasks")
 @EntityListeners(AuditingEntityListener.class)
-public class User {
+public class Task {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long userId;
+	private Long taskId;
 	
-	@Column(nullable = false)
-	private String name;
+	@ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+	private User user;
 	
-	@Column(nullable = false)
-	private String surname;
+	private String taskDescription;
 	
-
-	@Column(nullable = false, length = 16)
-	private String codeFiscale;
-	
-	private LocalDate birthDate;
-	
-	@Column(nullable = false, unique = true)
-	private String email;
-	
-	private String profileImage;
-
-	private Boolean isActive;
+	@Min(value = 1, message = "La priorità minima è 1")
+    @Max(value = 3, message = "La priorità massima è 3")
+	@Column(columnDefinition = "INTEGER CHECK (task_priority >= 1 AND task_priority <=3)")
+	private Long taskPriority;
 	
 	@CreatedDate
-	private LocalDateTime createdAt;
+	private LocalDateTime insertDateTime;
 	
-	private LocalDateTime deletedAt;
+	private LocalDateTime endDateTime;
+	
+	@Column
+	private Boolean isRepeated;
 	
 	@LastModifiedDate
 	private LocalDateTime updatedAt;
-	
-	
-	
-	@OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
-	private List<Task> userTrasks;
-	
-	
 }
